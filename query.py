f5740a1a40061a2dbe8e4e611632b7498c74b61e
Iris Ansorge <iansorge@emich.edu>
2023-12-01 19:43:45 +0000

    feature: added the option to list all the movie names
    added a 'list' option in the menu that will list all the movie names
    
    issue #2

--- query.py
+++ query.py
@@ -2,9 +2,23 @@ from movies import Movies
 
 movies = Movies('./movies.txt')
 
+def list_names():
+    for k in movies._movies:
+        print(k["name"])
+    print("\n")
+
+
 print("q: quit")
+print("list: print all the movie names")
 menu_input = input()
+
+if menu_input == "list":
+    list_names()
 
 while(menu_input != "q"):
     print("q: quit")
+    print("list: print all the movie names")
     menu_input = input()
+
+    if menu_input == "list":
+        list_names()
