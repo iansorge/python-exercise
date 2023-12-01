797f96859e56214fd0c5e9161bf9490e7823ae5a
Iris Ansorge <iansorge@emich.edu>
2023-12-01 20:36:03 +0000

    feature: added a search option to the menu that searches for movies by name
    added an option 'sn' to the menu that will prompt the user for a search term then search for movies with the term in their name
    
    issue #3

--- query.py
+++ query.py
@@ -2,23 +2,49 @@ from movies import Movies
 
 movies = Movies('./movies.txt')
 
+
+#defining a method to list all the movie names
 def list_names():
     for k in movies._movies:
         print(k["name"])
-    print("\n")
+
+#defining a method to search for movies with a search term in their name
+def search_names(term):
+    for k in movies._movies:
+        if(term.lower() in k["name"].lower()):
+            print(k["name"])
 
 
+#printing the menu options
 print("q: quit")
+print("sn: search movie names")
 print("list: print all the movie names")
+
+#storing the user's input ina  variable
 menu_input = input()
 
+
+#if the user enters 'list' list all the movie names
 if menu_input == "list":
     list_names()
 
+#if the user enters 'sn' prompt the user for a search term then search for movies with that term in their names
+if menu_input == "sn":
+    print("Enter a search term: ")
+    search_term = input()
+    search_names(search_term)
+
+
+#using a while loop to repeatedly prompt the user for input until they enter 'q'
 while(menu_input != "q"):
     print("q: quit")
+    print("sn: search movie names")
     print("list: print all the movie names")
     menu_input = input()
 
     if menu_input == "list":
         list_names()
+    if menu_input == "sn":
+        print("Enter a search term: ")
+        search_term = input()
+        search_names(search_term)
